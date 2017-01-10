7e1cd4e5f9c45310646958fd5d8d37c89fd37f3b
bestsamcn <645298225@qq.com>
2017-01-10 10:36:32 +0800

    完成项目基本搭建

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports=function(grunt){ 
+    //任务配置 
+    grunt.initConfig({ 
+        watch:{
+            template:{
+                files:['views/**','public/**'],
+                options:{
+                    livereload:35730,
+                    delay:1000
+                }
+            }
+        },
+        open: {
+            all: {
+                path: 'http://localhost:3000'
+            },
+            file : {
+              path : '/etc/hosts'
+            }
+        },
+        nodemon:{dev:{
+                script:'bin/www',
+                options:{
+                    env:{
+                        port:3000
+                    }
+                }
+            }
+        },
+        concurrent:{
+            tasks:['nodemon','watch','open'],
+            options:{
+               logConcurrentOutput:true
+            }
+        }
+    }); 
+    //载入任务 
+    grunt.loadNpmTasks('grunt-contrib-watch'); 
+    grunt.loadNpmTasks('grunt-nodemon'); 
+    grunt.loadNpmTasks('grunt-concurrent'); 
+    grunt.loadNpmTasks('grunt-open'); 
+    //注册任务 
+    grunt.registerTask('default',['concurrent']); 
+} 
