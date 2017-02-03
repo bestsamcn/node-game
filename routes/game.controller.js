73be0d6ab1d97095c18a44cb705c74255aeffa95
bestsamcn <645298225@qq.com>
2017-02-03 11:32:28 +0800

    某些修复

--- routes/game.controller.js
+++ routes/game.controller.js
@@ -9,44 +9,44 @@ var globalConfig = require('../config');
 /**
  * 添加游戏
  */
-router.get('/addGame/:channelId', routerOnlyForAdmin, function(req, res){
+router.get('/addGame/:channelId', routerOnlyForAdmin, function(req, res) {
 	var _channel_id = req.params.channelId;
-	if(!_channel_id || _channel_id.length !== 24){
+	if (!_channel_id || _channel_id.length !== 24) {
 		res.redirect('back');
 		res.end();
 		return;
 	}
 	var rcookies = req.cookies.NODESESSIONID;
-	var _getChannelDetail = function(){
+	var _getChannelDetail = function() {
 		var defer = Q.defer();
-		R.request('http://'+globalConfig.host+':'+globalConfig.port+'/api/admin/getChannelDetail',{
-			method:'get',
-			params:{
-				channelId:_channel_id
+		R.request('http://' + globalConfig.host + ':' + globalConfig.port + '/api/admin/getChannelDetail', {
+			method: 'get',
+			params: {
+				channelId: _channel_id
 			},
-			cookies:{
-				NODESESSIONID:rcookies
+			cookies: {
+				NODESESSIONID: rcookies
 			},
-			dataType:'json'
-		}).then(function(rdata){
+			dataType: 'json'
+		}).then(function(rdata) {
 			var data = JSON.parse(rdata.body);
-			if(data.retCode !== 0){
+			if (data.retCode !== 0) {
 				res.redirect('back');
 				res.end();
 				return;
 			}
 			defer.resolve(data.data);
-		},function(){
+		}, function() {
 			res.redirect('back');
 			res.end();
 		});
 		return defer.promise;
 	}
-	var _sendHtml = function(_channelDetail){
-		res.render('tpl/game/addGame',{
-			routerName:'/game/addGame',
-			title:'添加游戏',
-			channelDetail:_channelDetail
+	var _sendHtml = function(_channelDetail) {
+		res.render('tpl/game/addGame', {
+			routerName: '/game/addGame',
+			title: '添加游戏',
+			channelDetail: _channelDetail
 		});
 	}
 	_getChannelDetail().then(_sendHtml);
@@ -55,101 +55,101 @@ router.get('/addGame/:channelId', routerOnlyForAdmin, function(req, res){
 /**
  * 游戏列表
  */
- router.get('/:channelId', routerOnlyForAdmin, function(req, res){
- 	var _channel_id = req.params.channelId;
-	if(!_channel_id || _channel_id.length !== 24){
+router.get('/:channelId', routerOnlyForAdmin, function(req, res) {
+	var _channel_id = req.params.channelId;
+	if (!_channel_id || _channel_id.length !== 24) {
 		res.redirect('back');
 		res.end();
 		return;
 	}
 	var rcookies = req.cookies.NODESESSIONID;
-	var _getChannelDetail = function(){
+	var _getChannelDetail = function() {
 		var defer = Q.defer();
-		R.request('http://'+globalConfig.host+':'+globalConfig.port+'/api/admin/getChannelDetail',{
-			method:'get',
-			params:{
-				channelId:_channel_id
+		R.request('http://' + globalConfig.host + ':' + globalConfig.port + '/api/admin/getChannelDetail', {
+			method: 'get',
+			params: {
+				channelId: _channel_id
 			},
-			cookies:{
-				NODESESSIONID:rcookies
+			cookies: {
+				NODESESSIONID: rcookies
 			},
-			dataType:'json'
-		}).then(function(rdata){
+			dataType: 'json'
+		}).then(function(rdata) {
 			var data = JSON.parse(rdata.body);
-			console.log(data,'fffffffffffff')
-			if(data.retCode !== 0){
+			console.log(data, 'fffffffffffff')
+			if (data.retCode !== 0) {
 				res.redirect('back');
 				res.end();
 				return;
 			}
 			defer.resolve(data.data);
-		},function(){
+		}, function() {
 			res.redirect('back');
 			res.end();
 		});
 		return defer.promise;
 	}
-	var _sendHtml = function(_channelDetail){
-		res.render('tpl/game/index',{
-			routerName:'/game/index',
-			title:'游戏列表',
-			channelDetail:_channelDetail
+	var _sendHtml = function(_channelDetail) {
+		res.render('tpl/game/index', {
+			routerName: '/game/index',
+			title: '游戏列表',
+			channelDetail: _channelDetail
 		});
 	}
 	_getChannelDetail().then(_sendHtml);
- });
+});
 
 /**
  * 游戏编辑
  * @param  { mode:String } 模式标识
  * @param  { id:String } 游戏id
  */
-router.get('/editGame/:mode/:id', routerOnlyForAdmin, function(req, res){
+router.get('/editGame/:mode/:id', routerOnlyForAdmin, function(req, res) {
 	var _game_mode = req.params.mode;
- 	var _game_id = req.params.id;
- 	if(!_game_mode || (_game_mode !== '1' && _game_mode !== '2')){
+	var _game_id = req.params.id;
+	if (!_game_mode || (_game_mode !== '1' && _game_mode !== '2')) {
 		res.redirect('back');
 		res.end();
 		return;
 	}
-	if(!_game_id || _game_id.length !== 24){
+	if (!_game_id || _game_id.length !== 24) {
 		res.redirect('back');
 		res.end();
 		return;
 	}
 	var rcookies = req.cookies.NODESESSIONID;
-	var _getGameDetail = function(){
+	var _getGameDetail = function() {
 		var defer = Q.defer();
-		R.request('http://'+globalConfig.host+':'+globalConfig.port+'/api/game/getGameDetail',{
-			method:'get',
-			params:{
-				mode:_game_mode,
-				gameId:_game_id
+		R.request('http://' + globalConfig.host + ':' + globalConfig.port + '/api/game/getGameDetail', {
+			method: 'get',
+			params: {
+				mode: _game_mode,
+				gameId: _game_id
 			},
-			cookies:{
-				NODESESSIONID:rcookies
+			cookies: {
+				NODESESSIONID: rcookies
 			},
-			dataType:'json'
-		}).then(function(rdata){
+			dataType: 'json'
+		}).then(function(rdata) {
 			var data = JSON.parse(rdata.body);
-			if(data.retCode !== 0){
+			if (data.retCode !== 0) {
 				res.redirect('back');
 				res.end();
 				return;
 			}
 			data.data.mode = _game_mode;
 			defer.resolve(data.data);
-		},function(){
+		}, function() {
 			res.redirect('back');
 			res.end();
 		});
 		return defer.promise;
 	}
-	var _sendHtml = function(_gameDetail){
-		res.render('tpl/game/editGame',{
-			routerName:'/game/editGame',
-			title:'修改游戏',
-			gameDetail:_gameDetail
+	var _sendHtml = function(_gameDetail) {
+		res.render('tpl/game/editGame', {
+			routerName: '/game/editGame',
+			title: '修改游戏',
+			gameDetail: _gameDetail
 		});
 	}
 	_getGameDetail().then(_sendHtml);
