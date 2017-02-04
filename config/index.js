47b30516673a180dc2064883e81d000ddd4b80f2
bestsamcn <645298225@qq.com>
2017-02-04 19:03:54 +0800

    修改全局配置

--- config/index.js
+++ config/index.js
@@ -1,11 +1,13 @@
 var NODE_ENV = process.env.NODE_ENV || 'development',
 	NODE_HOST = process.env.NODE_HOST || '127.0.0.1',
 	NODE_PORT = process.env.NODE_PORT || 3030,
-	NODE_DATABASE = process.env.NODE_SECRECT || 'game',
 	NODE_SECRECT = process.env.NODE_SECRECT || 'node-game',
 	NODE_COOKIEID = 'NODESESSIONID',
 	NODE_REDIS_PORT = process.env.NODE_REDIS_PORT || 6379,
-	TEMPLATE_CACHE = ( NODE_ENV !== 'development' );
+	TEMPLATE_CACHE = ( NODE_ENV !== 'development' ),
+	MONGO_DATABASE = process.env.MONGO_DATABASE || 'game',
+	MONGO_USER = process.env.MONGO_USER || 'game',
+	MONGO_PASSWORD = process.env.MONGO_PASSWORD || '123123';
 var _config = {
 	host:NODE_HOST,
 	port:NODE_PORT,
@@ -25,8 +27,10 @@ var _config = {
 	    }
 	},
 	mongoConfig:{
-		mongodb: 'mongodb://'+NODE_HOST+'/'+NODE_DATABASE,
-	    database: NODE_DATABASE,
+		// mongoose.connect('mongodb://username:password@host:port/database?options...');
+		// mongodb: 'mongodb://admin:123123@10.28.5.197/swyc',
+		mongodb: 'mongodb://'+MONGO_USER+':'+MONGO_PASSWORD+'@'+NODE_HOST+'/'+MONGO_DATABASE,
+	    database: MONGO_DATABASE,
 	    server: NODE_HOST
 	},
 	authSecret:'080994c0cce4b706',
